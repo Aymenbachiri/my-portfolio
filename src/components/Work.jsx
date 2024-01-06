0b76314941bceb2f96310818c84a34afc12b4af0
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-01-05 23:20:19 -0800

    second commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,9 @@
 import weather from "../assets/projects/Weather-App.png";
 import ecommerce from "../assets/projects/Ecommerce-app.png";
+import todo from "../assets/projects/To-Do-App.png";
+import chat from "../assets/projects/Chat-App.png";
+import blog from "../assets/projects/Blog-App.png";
+import food from "../assets/projects/Food-App.png";
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-white bg-[#0a192f]">
@@ -48,6 +52,36 @@ const Work = () => {
             </div>
           </div>
           <div
+            style={{ backgroundImage: `url(${todo})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md
+              flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                To-Do App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://aymen-todo-app.netlify.app/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Aymenbachiri/todo-app">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
             style={{ backgroundImage: `url(${ecommerce})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md
               flex justify-center text-center items-center mx-auto content-div"
@@ -67,6 +101,96 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/Aymenbachiri/Ecommerce-app">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${chat})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md
+              flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Chat App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://aymen-chat-app.netlify.app/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Aymenbachiri/chat-app">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${blog})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md
+              flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Blog App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://aymen-blog-app.netlify.app/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Aymenbachiri/blog-app">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${food})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md
+              flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Food App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://aymen-eats.netlify.app/">
+                  <button
+                    className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                  >
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Aymenbachiri/food-app">
                   <button
                     className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
